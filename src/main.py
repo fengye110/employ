1336e942fa88446ddd7c35f72593a232fb839717
fengye <junyiao110@gmail.com>
2014-05-27 04:59:31 +0800

    listwidget

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ class MyDialog(QDialog):
     def __init__(self, parent=None):  
         super(MyDialog, self).__init__(parent)  
         self.MyTable = QTableWidget(4,3)  
+        #self.MyTable.setEditTriggers(QAbstractItemView.CurrentChanged) 
         self.MyTable.setHorizontalHeaderLabels(['姓名','身高','体重'])  
           
         newItem = QTableWidgetItem("松鼠")  
@@ -16,6 +17,27 @@ class MyDialog(QDialog):
           
         newItem = QTableWidgetItem("60g")  
         self.MyTable.setItem(0, 2, newItem)   
+
+
+        self.MyCombo = QComboBox()  
+        self.MyCombo.addItem("√")  
+        self.MyCombo.addItem("×")           
+        self.MyTable.setCellWidget(1,0,self.MyCombo)  
+
+
+        lv = QListView()  
+        itemmode = QStandardItemModel()
+        itemmode.appendRow(QStandardItem("a"))
+        itemmode.appendRow(QStandardItem("b"))
+        lv.setModel(itemmode)
+        lv.setFixedSize(200,300)
+        self.MyTable.setCellWidget(1,2,lv)  
+
+
+        lw = QListWidget()  
+        lw.addItem("a")
+        lw.addItem("b")
+        self.MyTable.setCellWidget(2,1,lw)  
         
         layout = QHBoxLayout()  
         layout.addWidget(self.MyTable)  
